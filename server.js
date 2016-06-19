bd0cf3c7eb9bfbd3352f52b47eb9ba19eca16143
Adam Lessen <lessena@gmail.com>
2016-06-18 17:06:30 -0700

    (feat) Add passport strategy for to auth via fb tokens from clients

--- server.js
+++ server.js
@@ -6,11 +6,28 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 const express = require('express');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('express-session');
 
 const app = express();
 
+// Configure passport strategy
+require('./config/passport')(app);
+
+
+// Auth routes
+require('./routes/auth-routes')(app);
+
 // API routes
 require('./routes/api-routes')(app);
+
+// Wildcard route
+app.get('*', function(req, res) {
+  res.send(404);
+});
 
 app.listen(Number(process.env.PORT), process.env.HOST, () => {
   console.log('listening *:' + process.env.PORT);
