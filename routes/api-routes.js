44cce10d50d43569e384468e22c84f232b6f77ce
Adam Lessen <lessena@gmail.com>
2016-06-20 20:00:27 -0700

    Add API route for posting to places

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,3 +1,28 @@
 module.exports = (app) => {
+  app.post('/api/users/:id/places', (req, res) => {
+    const userId = req.params.id;
+    const { name, lat, lng } = req.body.location;
 
+    console.log('USERID-----------', userId);
+    console.log('data coming back from place post--------------', req.body);
+    console.log('data is--------------', name, lat, lng);
+    /**
+      Places
+        .findOrCreate({where: {name: name}, defaults: {lat: lat, lng: lng})
+        .spread(function(place, created) {
+          console.log(place.get({
+            plain: true
+          }))
+          console.log(created)
+          UserPlaces --- upsert 
+            .findOrCreate({where: {placeId:place.id}, defaults: {userId: userId, note:req.body.note, videoUrl:'', pictureUrl: ''}
+            .spread(function(userPlace, created) {
+              res.send(201)
+            })
+  res.send(202)
+        })
+
+    */
+    res.send(200);
+  });
 };
