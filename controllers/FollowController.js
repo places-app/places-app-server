523ef7829ffcb4f2c8c202526090ff3e6e584983
Andrew Phavichitr <andrew.phavichitr@gmail.com>
2016-06-23 10:53:33 -0700

    Create follow controller

--- controllers/FollowController.js
+++ controllers/FollowController.js
@@ -1,3 +1,17 @@
-exports.followUser = function (req, res) {
-  console.log(req.params.userId);
+const Follow = require('../models').follow;
+
+exports.followUser = (req, res) => {
+  Follow.create({
+    id: 1,
+    userId: req.params.userId,
+    followedId: req.body.followedId,
+    following: true,
+  })
+  .then((follow) => {
+    console.log(`Successfuly inserted into Follows table for user ${follow.userId}`);
+    res.send(200);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 };
