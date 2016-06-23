7c6a7d53a7cd545e31f5ead1314869205db67af0
heysep <sdvakili@gmail.com>
2016-06-23 14:47:25 -0700

    Implement user controller methods to persist user after login

--- config/passport.js
+++ config/passport.js
@@ -4,28 +4,42 @@ if (process.env.NODE_ENV === 'development') {
 } else {
   require('dotenv').config({ path: './env/production.env' });
 }
-const passport = require('passport');
 
+const passport = require('passport');
 const FacebookTokenStrategy = require('passport-facebook-token');
 const session = require('express-session');
 
-passport.serializeUser((user, done) => done(null, user));
+const userController = require('../controllers/userController');
 
+passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
 
 passport.use(new FacebookTokenStrategy({
   clientID: process.env.FB_CLIENT_ID,
   clientSecret: process.env.FB_CLIENT_SECRET,
 }, (accessToken, refreshToken, profile, done) => {
-  /** this will be replaced with findOrCreate
-  */
-  const user = {
-    email: profile.emails[0].value,
-    name: `${profile.name.givenName} ${profile.name.familyName}`,
-    id: profile.id,
-    token: accessToken,
-  };
-  return done(null, user);
+  userController.getUser(profile.id)
+    .then((user) => {
+      if (user) {
+        return done(null, user.dataValues);
+      }
+      const newUser = {
+        fbId: profile.id,
+        email: profile.emails[0].value,
+        name: profile.displayName,
+        imageUrl: profile.photos[0].value,
+        accessToken,
+        refreshToken,
+      };
+      return userController.addUser(newUser)
+        .then((createdUser) => done(null, createdUser.dataValues))
+        .catch((err) => {
+          throw new Error(err);
+        });
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
 }));
 
 module.exports = function passportConfig(app) {
@@ -38,6 +52,3 @@ module.exports = function passportConfig(app) {
   app.use(passport.initialize());
   app.use(passport.session());
 };
-
-  // app.use(session({ secret: 'fred', resave: false, saveUninitialized: false }));
-
