7303d408a200b9d3a31917359b4e951bda266f2f
Adam Lessen <lessena@gmail.com>
2016-06-23 14:34:10 -0700

    (refactor) Modularize place controller

---
+++ controllers/placeController.js
@@ -0,0 +1,35 @@
+const Places = require('../models').place;
+const UserPlaces = require('../models').userPlaces;
+
+module.exports = {
+  insertPlace: (req, res) => {
+    const userId = req.params.id;
+    const { name, lat, lng } = req.body.location;
+    const note = req.body.note;
+    console.log('USERID-----------', userId);
+    console.log('data coming back from place post--------------', req.body);
+    console.log('data is--------------', name, lat, lng, note);
+
+    Places
+      .findOrCreate({
+        where: { name },
+        defaults: { lat, lng },
+      })
+      .spread((place, created) => {
+        console.log(place.get({
+          plain: true,
+        }));
+        console.log(created);
+        UserPlaces // --- upsert
+          .findOrCreate({
+            where: { placeId: place.id },
+            defaults: { userId, note, videoUrl: '', pictureUrl: '' },
+          })
+          .spread((userPlace, newEntry) => {
+            console.log('DID I MAKE IT HERE---------------');
+            console.log('newEntry---------------', newEntry);
+            return newEntry ? res.send(201) : res.send(202);
+          });
+      });
+  },
+};
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,37 +1,5 @@
-const Places = require('../models').place;
-const UserPlaces = require('../models').userPlaces;
-
-console.log('functions inside API ROUTES:', Places, UserPlaces);
-
+const placeController = require('../controllers/placeController');
 
 module.exports = (app) => {
-  app.post('/api/users/:id/places', (req, res) => {
-    const userId = req.params.id;
-    const { name, lat, lng } = req.body.location;
-    const note = req.body.note;
-    console.log('USERID-----------', userId);
-    console.log('data coming back from place post--------------', req.body);
-    console.log('data is--------------', name, lat, lng, note);
-
-    Places
-      .findOrCreate({
-        where: { name },
-        defaults: { lat, lng },
-      })
-      .spread((place, created) => {
-        console.log(place.get({
-          plain: true,
-        }));
-        console.log(created);
-        UserPlaces // --- upsert
-          .findOrCreate({
-            where: { placeId: place.id },
-            defaults: { userId, note, videoUrl: '', pictureUrl: '' },
-          })
-          .spread((userPlace, created2) => (
-            created2 ? res.send(201) : res.send(202)
-          ));
-      });
-    res.send(500);
-  });
+  app.post('/api/users/:id/places', placeController.insertPlace);
 };
