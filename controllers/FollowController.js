b0b1c050f9b954cdd210a28995d75db29c6f23e8
Andrew Phavichitr <andrew.phavichitr@gmail.com>
2016-06-23 19:24:28 -0700

    Update follow controller

--- controllers/FollowController.js
+++ controllers/FollowController.js
@@ -1,16 +1,18 @@
 const Follow = require('../models').follow;
 
-exports.followUser = (req, res) => {
-  Follow.create({
-    userId: req.params.userId,
-    followedId: req.body.followedId,
-    following: true,
-  })
-  .then((follow) => {
-    console.log(`Successfuly inserted into Follows table for user ${follow.userId}`);
-    res.send(200);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+module.exports = {
+  followUser: (req, res) => {
+    Follow.create({
+      userId: req.params.userId,
+      followedId: req.body.followedId,
+      following: true,
+    })
+    .then((follow) => {
+      console.log(`Successfuly inserted into Follows table for user ${follow.userId}`);
+      res.send(200);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  },
 };
