2d8f46fa32bf2807c19010533dab2994f95168e6
heysep <sdvakili@gmail.com>
2016-06-25 01:49:51 -0700

    Prepend /api/ to auth route

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,10 +2,8 @@ const userController = require('../controllers/userController');
 const passport = require('passport');
 
 module.exports = (app) => {
-  app.get('/auth/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
-    userController.getLoginPayLoad(req.user.fbId, payLoad => {
-      res.send(payLoad);
-    });
+  app.get('/api/auth/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+    userController.getLoginPayLoad(req, res);
   });
   app.get('/logout', (req, res) => {
     console.log('Logging out...: ');
