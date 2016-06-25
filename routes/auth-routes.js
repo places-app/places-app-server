9d7d6dc3d9dd83d1748ab6e21297a051eb3c25e1
heysep <sdvakili@gmail.com>
2016-06-25 14:39:37 -0700

    Remove /api/ from auth route

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,7 +2,7 @@ const userController = require('../controllers/userController');
 const passport = require('passport');
 
 module.exports = (app) => {
-  app.get('/api/auth/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  app.get('/auth/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
     userController.getLoginPayLoad(req, res);
   });
   app.get('/logout', (req, res) => {
