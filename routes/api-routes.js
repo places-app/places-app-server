3ea1f37505d6c4f1ae405eacc93db7ec34d3e098
heysep <sdvakili@gmail.com>
2016-06-24 17:37:15 -0700

    Add api route for get places

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,7 @@ const placeController = require('../controllers/placeController');
 const followController = require('../controllers/FollowController');
 
 module.exports = (app) => {
+  app.get('/api/users/:userId/places', placeController.getPlaces);
   app.post('/api/users/:id/places', placeController.insertPlace);
   app.post('/api/users/:userId/follows', followController.followUser);
 };
