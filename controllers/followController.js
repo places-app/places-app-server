a4d8c4b4d583e5f6327e9db773cac92967991ee1
Andrew Phavichitr <andrew.phavichitr@gmail.com>
2016-06-24 19:34:10 -0700

    Merge with upstream master

--- controllers/followController.js
+++ controllers/followController.js
@@ -1,18 +1,53 @@
 const Follow = require('../models').follow;
+const User = require('../models').user;
 
 module.exports = {
   followUser: (req, res) => {
-    Follow.create({
-      userId: req.params.userId,
-      followedId: req.body.followedId,
-      following: true,
+    // add unfollow
+    Follow.findOrCreate({
+      where: {
+        userId: req.params.userId,
+        followedId: req.body.followedId,
+        following: true,
+      },
     })
     .then((follow) => {
       console.log(`Successfuly inserted into Follows table for user ${follow.userId}`);
       res.send(200);
     })
     .catch((error) => {
+      // Add error handling and res status
       console.log(error);
+    });
+  },
+  getFollows: (req, res) => {
+    Follow.findAll({
+      where: {
+        userId: req.params.userId,
+        following: true,
+      },
+      raw: true,
+    })
+    .then((follows) => {
+      const allFollows = [];
+      follows.forEach((follow) => {
+        allFollows.push(follow.followedId);
+      });
+      User.findAll({
+        where: {
+          id: allFollows,
+        },
+        attributes: ['id', 'name', 'currLat', 'currLng', 'prevLat', 'prevLng', 'repCount'],
+        raw: true,
+      })
+      .then((followedUsers) => {
+        console.log(`Successfuly fetched from Follows table for user ${req.params.userId}`);
+        res.send(followedUsers);
+      })
+      .catch((error) => {
+        // Add error handling and res status
+        console.log(error);
+      });
     });
   },
 };
