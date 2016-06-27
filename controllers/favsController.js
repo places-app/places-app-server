7f408a759582a571b013cc5b95be6b3625b9d1d2
Adam Lessen <lessena@gmail.com>
2016-06-27 15:53:50 -0700

    (feat) Verify favController sends back the correct JSON data

--- controllers/favsController.js
+++ controllers/favsController.js
@@ -3,29 +3,26 @@ const Favs = require('../models').fav;
 
 module.exports = {
   insertFav: (req, res) => {
-    // will have: req.body.
-    // needs to have userPlaceId, placeId, userId is on params
     const userId = req.param('userId');
     const { userPlaceId, placeId } = req.body;
-    // Favs.findOrCreate({where placeId})
-    // consider upsert for setting starred to false
+    console.log('----------------------------', userId, userPlaceId, placeId);
     Favs
       .findOrCreate({
         where: { placeId, userId },
         defaults: { userPlaceId, starred: 't' },
       })
-      .spread((fav, created) => created ? res.send(201) : res.send(202));
+      .spread((fav, created) => { if (created) { res.send(201); } else { res.send(202); } });
   },
   getAllFavs: (req, res) => {
     const userId = req.param('userId');
     console.log('USER ID-----------', userId);
     Favs.
       findAll({
-        where: { userId, starred:'t' },
+        where: { userId, starred: 't' },
       })
       .then(results => {
         console.log('results is: ', results[0].userId);
-        resultObj = results.map(fav=>({userPlaceId:fav.userPlaceId}));
+        const resultObj = results.map(fav => ({ userPlaceId: fav.userPlaceId }));
         res.send(resultObj);
       });
   },
