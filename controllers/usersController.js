3a772b854bd30506ddb0faa079a0a591a0cff3e3
HeroProtagonist <jstepper91@gmail.com>
2016-06-26 20:50:20 -0700

    Implement users API route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,74 +4,35 @@ const Follow = db.follow;
 
 module.exports = {
   getUsers: function getUsers(req, res) {
-    const userId = 3//req.user.id;
-    console.log(userId);
-    // console.log(req.params)
-    // res.send(200);
+    const userId = req.user.id;
     return Follow.findAll({
-        attributes: ['followedId'],
+      attributes: ['followedId'],
+      where: {
+        userId,
+      },
+      raw: true,
+    })
+    .then((userFollows) => {
+      const ids = userFollows.map((user) => user.followedId);
+      ids.push(userId);
+      const query = {
+        attributes: ['id', 'email', 'name', 'imageUrl', 'repCount'],
         where: {
-          userId,
+          id: {
+            $notIn: ids,
+          },
         },
         raw: true,
-      })
-      .then((userFollows) => {
-        if (userFollows) {
-          console.log(userFollows);
-
-          let ids = userFollows.map((user) => user.followedId);
-          ids.push(userId);
-          
-
-          return User.findAll({
-            attributes: ['id', 'email', 'name', 'imageUrl', 'repCount'],
-            where: {
-              id: {
-                $notIn: ids,
-              },
-            },
-            raw: true,
-          })
-          .then((otherUsers) => {
-            console.log(otherUsers);
-          })
-
-          return user;
-        }
-        return false; // res send error
-      })
-      .catch((err) => {
-        throw new Error(err);
-      });
-  },
-  addUser: function addUser(user) {
-    return User.create(user)
-      .then((createdUser) => createdUser)
-      .catch((err) => {
-        throw new Error(err);
-      });
-  },
-  getLoginPayLoad: function getUser(req, res) {
-    const fbId = req.user.fbId;
-    return User.findOne({ where: { fbId }, raw: true })
-      .then((user) => {
-        if (user) {
-          const payLoad = {
-            id: user.id,
-            imageUrl: user.imageUrl,
-            name: user.name,
-            repCount: user.repCount,
-            email: user.email,
-            currLat: user.currLat,
-            currLng: user.currLng,
-            prevLat: user.prevLat,
-            prevLng: user.prevLng,
-          };
-          res.end(JSON.stringify(payLoad));
-        }
-      })
-      .catch((err) => {
-        throw new Error(err);
-      });
+      };
+      return new Promise((resolve) =>
+        resolve(User.findAll(query)
+      ));
+    })
+    .then((otherUsers) => {
+      res.send(otherUsers);
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
   },
 };
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,11 @@
 const placeController = require('../controllers/placeController');
 const followController = require('../controllers/followController');
+const usersController = require('../controllers/usersController');
 
 module.exports = (app) => {
   app.get('/api/users/:userId/places', placeController.getPlaces);
   app.post('/api/users/:id/places', placeController.insertPlace);
   app.post('/api/users/:userId/follows', followController.followUser);
   app.get('/api/users/:userId/follows', followController.getFollows);
+  app.get('/api/users', usersController.getUsers);
 };
