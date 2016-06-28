d04959bf05458498b67888c0228c84ad22c9f280
heysep <sdvakili@gmail.com>
2016-06-28 12:09:59 -0700

    Remove the index lookup on results array and use req.params

--- controllers/favsController.js
+++ controllers/favsController.js
@@ -3,7 +3,7 @@ const Favs = require('../models').fav;
 
 module.exports = {
   insertFav: (req, res) => {
-    const userId = req.param('userId');
+    const userId = req.params.userId;
     const { userPlaceId, placeId } = req.body;
     console.log('----------------------------', userId, userPlaceId, placeId);
     Favs
@@ -14,16 +14,16 @@ module.exports = {
       .spread((fav, created) => { if (created) { res.send(201); } else { res.send(202); } });
   },
   getAllFavs: (req, res) => {
-    const userId = req.param('userId');
+    const userId = req.params.userId;
     console.log('USER ID-----------', userId);
     Favs.
       findAll({
-        where: { userId, starred: 't' },
+        where: { userId, starred: 't' }, raw: true,
       })
       .then(results => {
-        console.log('results is: ', results[0].userId);
-        const resultObj = results.map(fav => ({ userPlaceId: fav.userPlaceId }));
-        res.send(resultObj);
+        console.log('User Favs:', results);
+        const resultArray = results.map(fav => ({ userPlaceId: fav.userPlaceId }));
+        res.send(resultArray);
       });
   },
 };
