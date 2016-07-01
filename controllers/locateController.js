5d3a19b131eab01f2e9a61245f7d46b6ec9ae5ca
Adam Lessen <lessena@gmail.com>
2016-06-30 22:33:43 -0700

    (feat) Add packages and logic to get user location data for client

--- controllers/locateController.js
+++ controllers/locateController.js
@@ -1,6 +1,16 @@
+const axios = require('axios');
+
+const serverUrl = 'http://localhost:3333/api/getLocations';
+
 module.exports = {
   getLocations: (req, res) => {
-    console.log('FOLLOWS BODY-----------------', req.body);
-    res.sendStatus(200);
+    const basicFollows = req.body.basicFollows;
+    console.log('FOLLOWS BODY-----------------', basicFollows);
+    axios.post(serverUrl, { basicFollows })
+    .then(response => {
+      console.log('RESPONSE FROM LOCATION SERVER--------', response.data);
+      res.send(response.data);
+    })
+    .catch(error => console.log(error));
   },
 };
