ec227f1800756bfd40306fde8b5cf32891e6fac6
Adam Lessen <lessena@gmail.com>
2016-07-02 11:37:57 -0700

    (cleanup) Remove console logs and update IP for deployed digital ocean

--- controllers/locateController.js
+++ controllers/locateController.js
@@ -1,14 +1,13 @@
 const axios = require('axios');
 
-const serverUrl = 'http://10.8.28.177:3333/api/getLocations';
+// digital ocean deployed IP
+const serverUrl = 'http://162.243.137.169:3333/api/getLocations';
 
 module.exports = {
   getLocations: (req, res) => {
     const basicFollows = req.body.basicFollows;
-    console.log('FOLLOWS BODY-----------------', basicFollows);
     axios.post(serverUrl, { basicFollows })
     .then(response => {
-      console.log('RESPONSE FROM LOCATION SERVER--------', response.data);
       res.send(response.data);
     })
     .catch(error => console.log(error));
