757673979c2be1daf7eb65264b6e69cb8e7b8791
heysep <sdvakili@gmail.com>
2016-07-03 22:17:26 -0700

    Implement controller to handle videourl updating requests

--- controllers/userPlaceController.js
+++ controllers/userPlaceController.js
@@ -8,21 +8,17 @@ module.exports = {
     const videoUrl = req.body.videoUrl;
     let oldUrl;
 
-    console.log('req.body', req.body);
+    console.log('updating userPlace with new url:', req.body);
 
     // update the video url
     UserPlace.findById(userPlaceId)
       .then((userPlace) => {
-        console.log('db url on db', userPlace.videoUrl);
-        // oldUrl = userPlace.videoUrl;
-        oldUrl = 'http://10.8.28.176:7000/dist/videos/e1f13e74e4324a23457d738a86c759e8';
-        console.log('hard coded url', oldUrl);
+        oldUrl = userPlace.videoUrl;
         return new Promise((resolve) =>
           resolve(userPlace.update({ videoUrl }))
         );
       })
-      .then((updatedUserPlace) => {
-        console.log('updated place:', updatedUserPlace);
+      .then(() => {
         const fileName = oldUrl.match(/\/([^/]*)$/)[1];
         const filePath = path.join(`${__dirname}./../dist/videos/${fileName}`);
         // delete file
@@ -30,7 +26,7 @@ module.exports = {
           if (err) {
             throw new Error(err);
           }
-          console.log('Video has been deleted:', filePath);
+          console.log('video file was deleted.', filePath);
         });
       })
       .catch((err) => {
@@ -38,6 +34,6 @@ module.exports = {
       });
 
     res.sendStatus(200);
-    res.end('video updated...');
+    res.end('video url was updated.');
   },
 };
